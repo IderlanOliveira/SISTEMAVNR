8f73f8c75f2e6c9749ae9506cfbf95b93701a8cd
Iderlan Oliveira <iderlanoliveira19@gmail.com>
2025-08-21 19:26:32 -0300

    Update login.js 1.5

--- login.js
+++ login.js
@@ -1,19 +1,23 @@
 const validCredentials = {
     '11765919401': '23030270ji',
     '09711439433': '988391077',  // Adicione novos usuários e senhas aqui
-    'user3': 'password3'
 };
 
-document.getElementById('loginButton').addEventListener('click', function() {
-    const user = document.getElementById('cpf').value;
-    const senha = document.getElementById('senha').value;
-    const loginError = document.getElementById('loginError');
-
+// Função de login
+funcion login(user,senha){
     if (validCredentials[user] && validCredentials[user] === senha) {
-        localStorage.setItem('authToken', 'loggendIn')
-        window.location.href = 'verificacao.html';
+        localStore.setItem("auth", "ok");
+        window.location.href = "verificacao.html;
     } else {
-        loginError.textContent = 'CPF ou SENHA incorretos. Tente novamente.';
+        alert("CPF ou SENHA incorretos!");
     }
-});
+}
 
+// Bloqueia navegação se não estiver logado
+function checkAuth(){
+    if(localStorage.getItem("auth") !== "ok") {
+        alert("Você precisa estar logado!");
+        window.location.href = "index.html";
+
+    }
+}
